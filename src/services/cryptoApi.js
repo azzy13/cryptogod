bb76e3e2011a356a92d93a3b7341396f03163cd7
azzy13 <azharhasan013@gmail.com>
2022-01-08 20:59:00 +0530

    fixes

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+  'x-rapidapi-key': 'e0b17a7171mshf473e7babfa78dfp1241dajsn70a97b5f15ab',
 };
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
@@ -22,7 +22,8 @@ export const cryptoApi = createApi({
 
     // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
     getCryptoHistory: builder.query({
-      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
     }),
 
     // Note: To access this endpoint you need premium plan
