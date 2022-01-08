c5a6add6aa5d8e3f59cab2b5c932a6c314088803
azzy13 <azharhasan013@gmail.com>
2022-01-08 21:07:41 +0530

    Fix2

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -18,23 +18,63 @@ const Homepage = () => {
 
   return (
     <>
-      <Title level={2} className="heading">Global Crypto Stats</Title>
+      <Title level={2} className='heading'>
+        Global Crypto Stats
+      </Title>
       <Row gutter={[32, 32]}>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={12}><Statistic title="Total Market Cap:" value={`$${millify(globalStats.totalMarketCap)}`} /></Col>
-        <Col span={12}><Statistic title="Total 24h Volume" value={`$${millify(globalStats.total24hVolume)}`} /></Col>
-        <Col span={12}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} /></Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Cryptocurrencies'
+            value={globalStats?.total}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Exchanges'
+            value={millify(globalStats?.totalExchanges)}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Market Cap:'
+            value={`$${millify(globalStats?.totalMarketCap)}`}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total 24h Volume'
+            value={`$${millify(globalStats?.total24hVolume)}`}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Cryptocurrencies'
+            value={globalStats?.total}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title='Total Markets'
+            value={millify(globalStats?.totalMarkets)}
+          />
+        </Col>
       </Row>
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">Top 10 Cryptos In The World</Title>
-        <Title level={3} className="show-more"><Link to="/cryptocurrencies">Show more</Link></Title>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Top 10 Cryptos In The World
+        </Title>
+        <Title level={3} className='show-more'>
+          <Link to='/cryptocurrencies'>Show more</Link>
+        </Title>
       </div>
       <Cryptocurrencies simplified />
-      <div className="home-heading-container">
-        <Title level={2} className="home-title">Latest Crypto News</Title>
-        <Title level={3}><Link to="/news">Show more</Link></Title>
+      <div className='home-heading-container'>
+        <Title level={2} className='home-title'>
+          Latest Crypto News
+        </Title>
+        <Title level={3}>
+          <Link to='/news'>Show more</Link>
+        </Title>
       </div>
       <News simplified />
     </>
