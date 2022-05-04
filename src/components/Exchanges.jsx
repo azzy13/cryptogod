2b0240f4eaa2cc6334762a044d2688db9c54ccee
azzy13 <azharhasan013@gmail.com>
2022-05-04 13:23:04 +0530

    link

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -4,7 +4,7 @@ function Exchanges() {
   return (
     <div>
       <a
-        href='https://www.kaggle.com/code/kaushikbajaj/cryptocurrency-prediction?scriptVersionId=93883328'
+        href='https://www.kaggle.com/code/kaushikbajaj/cryptocurrency-prediction/notebook?scriptVersionId=94631020'
         target='_blank'
         rel='noreferrer'
       >
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,7 +6,8 @@ import { useGetCryptosQuery } from '../services/cryptoApi';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import Loader from './Loader';
 
-const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+const demoImage =
+  'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -14,7 +15,10 @@ const { Option } = Select;
 const News = ({ simplified }) => {
   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
   const { data } = useGetCryptosQuery(100);
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
 
   if (!cryptoNews?.value) return <Loader />;
 
@@ -24,32 +28,55 @@ const News = ({ simplified }) => {
         <Col span={24}>
           <Select
             showSearch
-            className="select-news"
-            placeholder="Select a Crypto"
-            optionFilterProp="children"
+            className='select-news'
+            placeholder='Select a Crypto'
+            optionFilterProp='children'
             onChange={(value) => setNewsCategory(value)}
-            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
           >
-            <Option value="Cryptocurency">Cryptocurrency</Option>
-            {data?.data?.coins?.map((currency) => <Option value={currency.name}>{currency.name}</Option>)}
+            <Option value='Cryptocurency'>Cryptocurrency</Option>
+            {data?.data?.coins?.map((currency) => (
+              <Option value={currency.name}>{currency.name}</Option>
+            ))}
           </Select>
         </Col>
       )}
       {cryptoNews.value.map((news, i) => (
         <Col xs={24} sm={12} lg={8} key={i}>
-          <Card hoverable className="news-card">
-            <a href={news.url} target="_blank" rel="noreferrer">
-              <div className="news-image-container">
-                <Title className="news-title" level={4}>{news.name}</Title>
-                <img src={news?.image?.thumbnail?.contentUrl || demoImage} alt="" />
+          <Card hoverable className='news-card'>
+            <a href={news.url} target='_blank' rel='noreferrer'>
+              <div className='news-image-container'>
+                <Title className='news-title' level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt=''
+                />
               </div>
-              <p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
-              <div className="provider-container">
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className='provider-container'>
                 <div>
-                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="" />
-                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                    alt=''
+                  />
+                  <Text className='provider-name'>
+                    {news.provider[0]?.name}
+                  </Text>
                 </div>
-                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
+                <Text>
+                  {moment(news.datePublished).startOf('ss').fromNow()}
+                </Text>
               </div>
             </a>
           </Card>
